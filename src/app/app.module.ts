15f774f44ed966d0d1d92b946cb3fb14d38f361c
satenik <satenikmargaryanm@gmail.com>
2020-02-04 19:06:51 +0400

    Initial Commit.

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HomeComponent } from "./home/home.component";
+
+@NgModule({
+  declarations: [AppComponent, NavbarComponent, FooterComponent, HomeComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
