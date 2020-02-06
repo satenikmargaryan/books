01d8f09f897c7409ae609a55dffaebfed7242015
satenik <satenikmargaryanm@gmail.com>
2020-02-06 10:04:09 +0400

    Added Contact page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { FooterComponent } from "./footer/footer.component";
 import { HomeComponent } from "./home/home.component";
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, FooterComponent, HomeComponent],
+  declarations: [AppComponent, NavbarComponent, FooterComponent, HomeComponent, ContactComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
